6fda92fd15d0d900303314043ef67256821472de
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2022-12-18 13:06:08 +0100

    Log access of user agents

--- src/core.clj
+++ src/core.clj
@@ -1,27 +1,30 @@
 (ns core
   (:require [org.httpkit.server :as srv]
-            [hiccup2.core :refer [html]]))
+            [hiccup2.core :refer [html]])
+  (:import (java.time LocalDateTime)))
+
+(defn logged [user-agent-string]
+  (spit "user-agents.log" (str user-agent-string ";" (.toString (LocalDateTime/now)) "\n") :append true)
+  user-agent-string)
 
 (defn extract-instance-name [user-agent-string]
-  (println user-agent-string)
   (let [[_ version host] (re-find #"\((.*).?\;.?\+https:\/\/(.*)\/\)" user-agent-string)]
     (if (and version (.contains version "Mastodon"))
       host
       nil)))
 
 (defn extract-client-info [{:keys [headers]}]
-  (extract-instance-name (get headers "user-agent")))
+  (extract-instance-name (logged (get headers "user-agent"))))
 
 (defn handle-request [request]
   (let [host (extract-client-info request)]
     (if host
-      (let [bait (str "Users of Mastodon instance " host " are the smartest!")]
+      (let [bait (str "Why " host " is the best Mastodon instance to be on!")]
         {:headers {"content-type" "text/html"}
          :body
          (str (html [:html
                      [:head [:title bait]]
-                     [:body [:h1 bait]
-                      [:p (str "This is unbelievable! A recent study discovered, that " bait)]]]))})
+                     [:body [:h1 bait]]]))})
       {:status 301
        :headers {"Location" "https://javahippie.net"}})))
 
